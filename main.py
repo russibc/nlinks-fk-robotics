abe07f653ec23830344b3c0f0f61ccea89cc29bf
Bruna <bruna.cr@aluno.ifsc.edu.br>
2022-06-19 23:20:53 -0300

    Code refactoring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from robot import Robot
 
-def main():
-    rb = Robot(2)
 
-main()
+def main():
+    rb = Robot(3)
+
+
+if __name__ == "__main__":
+    main()
--- robot.py
+++ robot.py
@@ -14,23 +14,22 @@ class Robot:
             self.execute()
 
     def FK(self, th, L):
-        L1, L2, L3 = L
         Th1, Th2, Th3 = th
         x0 = 0.0
         y0 = 0.0
 
-        x1 = L1*math.cos(Th1)
-        y1 = L1*math.sin(Th1)
+        x1 = L[0]*math.cos(Th1)
+        y1 = L[0]*math.sin(Th1)
 
         if(self.links == 2):
-            x2 = x1 + L2*math.cos(Th1 + Th2)
-            y2 = y1 + L2*math.sin(Th1 + Th2)
+            x2 = x1 + L[1]*math.cos(Th1 + Th2)
+            y2 = y1 + L[1]*math.sin(Th1 + Th2)
         elif(self.links == 3):
-            x2 = x1 + L2*math.cos(Th1 + Th2)
-            y2 = y1 + L2*math.sin(Th1 + Th2)
+            x2 = x1 + L[1]*math.cos(Th1 + Th2)
+            y2 = y1 + L[1]*math.sin(Th1 + Th2)
 
-            x3 = x2 + L3*math.cos(Th1+Th2+Th3)
-            y3 = y2 + L3*math.sin(Th1+Th2+Th3)
+            x3 = x2 + L[2]*math.cos(Th1+Th2+Th3)
+            y3 = y2 + L[2]*math.sin(Th1+Th2+Th3)
 
         X = []
         if(self.links == 1):
@@ -42,97 +41,81 @@ class Robot:
 
         return X
 
+    def defineL(self):
+        L = []
+        while(len(L) != self.links):
+            L.append(0.5)
+        return L
+
+    def updateGraphic(self, text, fig, graph, p):
+        graph.set_data(p.T[0], p.T[1])
+        graph.set_linestyle('-')
+        graph.set_linewidth(5)
+        graph.set_marker('o')
+        graph.set_markerfacecolor('g')
+        graph.set_markeredgecolor('g')
+        graph.set_markersize(15)
+        fig.canvas.draw_idle()
+        text.set_text(
+            'x = '+str(round(p.T[0][self.links], 2))+', y =' + str(round(p.T[1][self.links], 2)))
+
     def execute(self):
         slider1_pos = 0
         slider2_pos = 0
         slider3_pos = 0
-        L1 = 0.5
-        L2 = 0.5
-        L3 = 0.5
-        L = [L1, L2, L3]
+
+        L = self.defineL()
+
         th = [0.0*math.pi, 0.0*math.pi, 0.0*math.pi]
 
         p = self.FK(th, L)
 
         fig, ax = plt.subplots()
-        plt.title('Forward Kinematics '+str(self.links)+' Link'+('s' if self.links > 1 else ''))
+        plt.title('Forward Kinematics '+str(self.links) +
+                  ' Link'+('s' if self.links > 1 else ''))
         plt.axis('equal')
         plt.subplots_adjust(left=0.1, bottom=0.25)
         plt.xlim([-2.5, 2.5])
         plt.ylim([-2.5, 2.5])
-
         plt.grid()
         graph, = plt.plot(p.T[0], p.T[1])
-
-        text = ax.text(.8, .8, 'x = '+str(p.T[0][self.links])+', y ='+ str(p.T[1][self.links]))
+        text = ax.text(.8, .8, 'x = '+str(
+            round(p.T[0][self.links], 2))+', y =' + str(round(p.T[1][self.links], 2)))
 
         def update_th1(slider_val):
             th[0] = slider_val
-
             p = self.FK(th, L)
-
-            text.set_text('x = '+str(round(p.T[0][self.links],2))+', y ='+ str(round(p.T[1][self.links],2)))
-
-            graph.set_data(p.T[0], p.T[1])
-            graph.set_linestyle('-')
-            graph.set_linewidth(5)
-            graph.set_marker('o')
-            graph.set_markerfacecolor('g')
-            graph.set_markeredgecolor('g')
-            graph.set_markersize(15)
-
-            fig.canvas.draw_idle()
+            self.updateGraphic(text, fig, graph, p)
 
         def update_th2(slider_val):
             th[1] = slider_val
-
             p = self.FK(th, L)
-
-            text.set_text('x = '+str(round(p.T[0][self.links],2))+', y ='+ str(round(p.T[1][self.links],2)))
-
-            graph.set_data(p.T[0], p.T[1])
-            graph.set_linestyle('-')
-            graph.set_linewidth(5)
-            graph.set_marker('o')
-            graph.set_markerfacecolor('g')
-            graph.set_markeredgecolor('g')
-            graph.set_markersize(15)
-
-            fig.canvas.draw_idle()
+            self.updateGraphic(text, fig, graph, p)
 
         def update_th3(slider_val):
             th[2] = slider_val
-
             p = self.FK(th, L)
+            self.updateGraphic(text, fig, graph, p)
 
-            text.set_text('x = '+str(round(p.T[0][self.links],2))+', y ='+ str(round(p.T[1][self.links],2)))
-
-            graph.set_data(p.T[0], p.T[1])
-            graph.set_linestyle('-')
-            graph.set_linewidth(5)
-            graph.set_marker('o')
-            graph.set_markerfacecolor('g')
-            graph.set_markeredgecolor('g')
-            graph.set_markersize(15)
-
-            fig.canvas.draw_idle()
-
-        slider1_pos = plt.axes([0.1, 0.13, 0.8, 0.03])      
-        threshold_slider1 = Slider(slider1_pos, 'th1', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
+        slider1_pos = plt.axes([0.1, 0.13, 0.8, 0.03])
+        threshold_slider1 = Slider(
+            slider1_pos, 'th1', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
         threshold_slider1.on_changed(update_th1)
 
         if(self.links == 2):
             slider2_pos = plt.axes([0.1, 0.09, 0.8, 0.03])
-            threshold_slider2 = Slider(slider2_pos, 'th2', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
+            threshold_slider2 = Slider(
+                slider2_pos, 'th2', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
             threshold_slider2.on_changed(update_th2)
         elif(self.links == 3):
-
             slider2_pos = plt.axes([0.1, 0.09, 0.8, 0.03])
-            threshold_slider2 = Slider(slider2_pos, 'th2', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
+            threshold_slider2 = Slider(
+                slider2_pos, 'th2', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
             threshold_slider2.on_changed(update_th2)
 
             slider3_pos = plt.axes([0.1, 0.05, 0.8, 0.03])
-            threshold_slider3 = Slider(slider3_pos, 'th3', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
+            threshold_slider3 = Slider(
+                slider3_pos, 'th3', -1.0*math.pi, 1.0*math.pi, 0.0*math.pi)
             threshold_slider3.on_changed(update_th3)
 
         graph.set_linestyle('-')
